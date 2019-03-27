e35829f13d0375a8a1921637fae7b8dec51cb8f8
Jack Robbins <jackrobbins1@gmail.com>
2019-03-27 17:03:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "pry"
 emotes = YAML.load_file('/home/fortunate-checkpoint-8501/emoticon-translator-chicago-web-career-040119/lib/emoticons.yml')
 binding.pry
 def load_library(path)
-  YAML.load_file("#{}")
+  YAML.load_file("#{path}")
 end
 
 def get_japanese_emoticon
